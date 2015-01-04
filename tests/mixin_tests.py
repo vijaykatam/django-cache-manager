8f71d2bfe72eb4f615f670d8e1bedaa77701a60a
Vijay Katam <vijay.katam@cerner.com>
2015-01-03 19:57:57 -0600

    remaining tests for mixins

--- tests/mixin_tests.py
+++ tests/mixin_tests.py
@@ -4,14 +4,17 @@ import hashlib
 from unittest import TestCase
 from mock import patch, Mock
 
-from django_cache_manager.mixins import CacheKeyMixin
+from django_cache_manager.mixins import (
+    CacheKeyMixin,
+    CacheInvalidateMixin
+)
 from django_cache_manager.model_cache_sharing.types import ModelCacheInfo
 from .models import Manufacturer
 
 
 @patch('django_cache_manager.mixins.model_cache_backend')
 @patch.object(CacheKeyMixin, 'sql')
-class CacheKeyMixinGenerateKeyTests(TestCase):
+class CacheKeyMixinTests(TestCase):
 
     def setUp(self):
         self.mixin = CacheKeyMixin()
@@ -19,7 +22,7 @@ class CacheKeyMixinGenerateKeyTests(TestCase):
         self.mixin.db = 'db'
 
     def test_consistent_key_generation(self, mock_sql, mock_model_cache):
-        "generate_key generates identical keys for repeated calls when sql and model are the same"
+        "Mixin generates identical keys for repeated calls when sql and model are the same"
         mock_sql.return_value = 'sql'
         key1 = self.mixin.generate_key()
         key2 = self.mixin.generate_key()
@@ -27,7 +30,7 @@ class CacheKeyMixinGenerateKeyTests(TestCase):
 
     @patch('django_cache_manager.mixins.uuid')
     def test_new_key_generation(self, mock_uuid, mock_sql, mock_model_cache):
-        "generate_key broadcasts key for the model when it is newly created"
+        "Mixin broadcasts key for the model when it is newly created"
         mock_model_cache.retrieve_model_cache_info.return_value = None
         mock_uuid4 = Mock(hex='unique_id')
         mock_uuid.uuid4.return_value = mock_uuid4
@@ -36,7 +39,7 @@ class CacheKeyMixinGenerateKeyTests(TestCase):
         mock_model_cache.broadcast_model_cache_info.assert_called_once_with(model_cache_info)
 
     def test_key_components(self, mock_sql, mock_model_cache):
-        "Ensure key created by generate_key is a hash of model_key, sql query and database name"
+        "Ensure key created by mixin is a hash of model_key, sql query and database name"
         mock_sql.return_value = 'sql'
         mock_model_cache.retrieve_model_cache_info.return_value = ModelCacheInfo(table_name=u'tests_manufacturer', table_key='unique_id')
         expected_key_value = hashlib.md5(u'unique_idsqldb').hexdigest()
@@ -58,3 +61,19 @@ class CacheKeyMixinGenerateKeyTests(TestCase):
         key, created = self.mixin.get_or_create_model_key()
         self.assertEquals(True, created)
         self.assertEquals(key, 'uuid')
+
+
+@patch('django_cache_manager.mixins.model_cache_backend')
+class CacheInvalidateMixinTests(TestCase):
+
+    def setUp(self):
+        self.mixin = CacheInvalidateMixin()
+        self.mixin.model = Manufacturer()
+
+    @patch('django_cache_manager.mixins.uuid')
+    def test_invalidate_model_cache(self, mock_uuid, mock_model_cache):
+        "Mixin broadcasts new model cache info when a model is invalidated"
+        mock_uuid4 = Mock(hex='unique_id')
+        mock_uuid.uuid4.return_value = mock_uuid4
+        self.mixin.invalidate_model_cache()
+        mock_model_cache.broadcast_model_cache_info.assert_called_once_with(ModelCacheInfo(table_name=u'tests_manufacturer', table_key='unique_id'))
