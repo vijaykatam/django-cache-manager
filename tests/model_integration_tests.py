2e27e5e23a1d5fec1beee95320d1ca255c679e3c
Vijay Katam <vijay.katam@cerner.com>
2015-01-08 22:39:52 -0600

    Add tests that verify no. of db queries for repeated calls

--- tests/model_integration_tests.py
+++ tests/model_integration_tests.py
@@ -6,9 +6,18 @@ Use models defined in tests t
 
 1. Black box tests - input model data,  retrieve, delete, update -assert that right data is retrieved
 """
+import django
 from django.forms.models import model_to_dict
 from django.core.exceptions import ObjectDoesNotExist
+from django.db import (
+    connection,
+    reset_queries
+)
 from django.test import TestCase
+if django.get_version() > '1.7':
+    from django.test import override_settings
+else:
+    from django.test.utils import override_settings
 
 from tests.models import(
     Car,
@@ -24,7 +33,10 @@ from tests.factories import(
 )
 
 
-class ModelIntegrationTests(TestCase):
+class ModelCRUDTests(TestCase):
+    """
+    Create, read, update and delete tests
+    """
 
     def setUp(self):
         self.manufacturers = ManufacturerFactory.create_batch(size=5)
@@ -40,6 +52,9 @@ class ModelIntegrationTests(TestCase):
         self.assertEquals(len(Manufacturer.objects.all()), len(Manufacturer.objects.all()))
 
     def test_update_manufacturer(self):
+        """
+        Cache manager returns most recently updated record even if it has been cached and an update happens after.
+        """        
         m = self.manufacturers[0]
         self.assertEquals(model_to_dict(m), model_to_dict(Manufacturer.cached_objects.get(id=m.id)))
         m.name = 'Toyota'
@@ -47,12 +62,53 @@ class ModelIntegrationTests(TestCase):
         self.assertEquals(model_to_dict(m), model_to_dict(Manufacturer.cached_objects.get(id=m.id)))
 
     def test_delete_manufacturer(self):
+        """
+        When a cached record is deleted Cache manager raises ObjectDoesNotExist when trying to retrieve
+        """        
         m = self.manufacturers[0]
         self.assertEquals(model_to_dict(m), model_to_dict(Manufacturer.cached_objects.get(id=m.id)))
         m.delete()
         with self.assertRaises(ObjectDoesNotExist):
             Manufacturer.cached_objects.get(id=m.id)
 
+
+class ModelCacheTests(TestCase):
     """
-    Add tests for bulk create and update
+    Test cache hits and misses
     """
+
+    def setUp(self):
+        self.manufacturers = ManufacturerFactory.create_batch(size=5)
+        reset_queries()
+
+    @override_settings(DEBUG=True)
+    def test_cache_hit(self):
+        """
+        Number of sql queries should be 1 when making the same query repeatedly.
+        """
+        for i in range(5):
+            len(Manufacturer.cached_objects.all())
+        self.assertEquals(1, len(connection.queries))
+
+    
+    def test_dummy_cache(self):
+        """
+        Number of sql queries is equal to the number of manager accesses when cache backend is dummy.
+        """        
+        CACHES = {
+            'default': {
+                'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+            },
+            'django_cache_manager.cache_backend': {
+                'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+            }
+        }       
+        with override_settings(DEBUG=True, CACHES=CACHES):
+            for i in range(5):
+                len(Manufacturer.cached_objects.all())
+            self.assertEquals(5, len(connection.queries))
+
+
+
+#TODO Add tests for bulk create and update
+
