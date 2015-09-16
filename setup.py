2be1d7cb698cd4fcaa8642b56b07315ec4efa95e
vk019637 <vijay.katam@cerner.com>
2015-09-15 20:12:51 -0500

    Add travis build for 1.8

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ readme = open('README.md').read()
 history = open('HISTORY.md').read().replace('.. :changelog:', '')
 
 requirements = [
-    'Django<1.8',
+    'Django<1.9',
 ]
 
 test_requirements = [
