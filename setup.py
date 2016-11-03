af5bb6564adb60b74f7542b1ad3ee897eef29a6d
Eugene Pankov <john.pankov@gmail.com>
2016-11-03 23:41:48 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ readme = open('README.md').read()
 history = open('HISTORY.md').read().replace('.. :changelog:', '')
 
 requirements = [
-    'Django',
+    'Django<1.11',
 ]
 
 test_requirements = [
